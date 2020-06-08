ee2d8710fd7ab61de291ac8bbd8e56ec8641a153
Agustin Gomez <gusmandoman@gmail.com>
2020-06-07 17:49:39 -0700

    Arch Progressive Fade and Brighten System

--- Assets/Scripts/Movement/Section.cs
+++ Assets/Scripts/Movement/Section.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Section : MonoBehaviour
 {
     public Lane[] lanes;
+    public int fadingArch;
+    public int lastFade;
     public EnemySpawner[] spawners;
+    public GameObject[] arches;
     [System.Serializable]
     public class MultiDimensionalInt
     {
-    public GameObject[] platforms;
+        public GameObject[] platforms;
     }
     public MultiDimensionalInt[] lanePlats;
     public MultiDimensionalInt[] lights;
     public bool sectionActive;
     public bool first;
+    public bool fading;
+    public bool alt;
+    public float intensityLev;
     public Color onLight;
     public Color offLight;
     public Material offColor;
@@ -29,6 +35,28 @@ public class Section : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        if(sectionActive)
+        {
+            if(!fading)
+            {
+                if(alt)
+                {
+                    StartCoroutine(FadeTo(1,.25f));
+                }
+                else
+                {
+                    StartCoroutine(FadeTo(0,.25f));
+                } 
+            }
+        }
+
+        else
+        {
+            
+        }
+    }
     public void activateSpawner()
     {
         if(sectionActive)
@@ -69,20 +97,26 @@ public class Section : MonoBehaviour
                 }
             }
         }
+
         else
         {
-             for (int i = 0; i < lanePlats.Length; i++)
-             {
+            for (int i = 0; i < lanePlats.Length; i++)
+            {
                 foreach(GameObject x in lanePlats[i].platforms)
                 {
-                        x.GetComponent<Renderer>().material = offColor;
+                    x.GetComponent<Renderer>().material = offColor;
                 }
                 
                 foreach(GameObject x in lights[i].platforms)
                 {
                     x.GetComponent<Light>().color = offLight;
                 }
-             }
+            }
+            for( int i = 0; i < arches.Length; i++)
+            {
+                arches[i].GetComponent<Renderer>().material.SetColor("_EmissionColor",new Color(1,1,1,1) * 0);
+            }
+        
         } 
     }
     public void deactivateSpawner()
@@ -113,5 +147,35 @@ public class Section : MonoBehaviour
         {
             tag = "Untagged";  
         }
+    }
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        fading = true;
+        intensityLev = arches[fadingArch].GetComponent<Renderer>().material.GetColor("_EmissionColor").a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            Color newColor = new Color(1, 1, 1, 1);
+            arches[fadingArch].GetComponent<Renderer>().material.SetColor("_EmissionColor",newColor * Mathf.Lerp(intensityLev,aValue,t));
+            yield return null;
+        }
+        fadingArch ++;
+
+        if(fadingArch == arches.Length)
+        {
+            fadingArch = 0;
+
+            if(!alt)
+            {
+                alt = true;
+            }
+            else
+            {
+                alt = false;
+            }
+            
+        }
+        fading = false;
+        
     }
 }
