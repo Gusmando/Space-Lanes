31412a28184b1fd0de9e3691faa422abc573c682
Agustin Gomez <gusmandoman@gmail.com>
2020-06-10 16:24:21 -0700

    Lob Gun/Enemy

---
+++ Assets/Scripts/Enemies/lobbingEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lobbingEnemy : EnemyMovement
+{
+    override public void Start()
+    {
+        base.Start();   
+    }
+
+    override public void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Guns/GunTypes/lobGun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lobGun : Gun
+{
+    public GameObject target;
+    private void Start() 
+    {
+        canShoot = true;
+        fullClip = clipSize;
+        auto = true;
+        target = GameObject.FindWithTag("Player");
+    }
+    public override void shoot()
+    { 
+        Vector3 directionEdit = new Vector3 (barrelLocation.position.x, target.transform.position.y,target.transform.position.z); 
+        Vector3 direction = directionEdit - barrelLocation.position;
+        direction = Vector3.Scale(direction, new Vector3(-1,-1,1));
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = lookRotation.eulerAngles;
+        barrelLocation.eulerAngles = rotation;
+        GameObject arrow = Instantiate(bullet,barrelLocation.position , barrelLocation.rotation);
+        Rigidbody rb = arrow.GetComponent<Rigidbody>();
+        arrow.GetComponent<Bullet>().damage = this.damage;
+        Vector3 temp = barrelLocation.eulerAngles;
+        barrelLocation.eulerAngles = new Vector3(-10,-temp.y,-temp.z);
+        rb.AddForce(barrelLocation.forward * (-1*bulletSpeed),ForceMode.Impulse);
+        StartCoroutine(shootingDelay(delayTime));
+        clipSize--;
+    }
+
+}
