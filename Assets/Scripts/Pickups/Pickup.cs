b442daef19b3173285444b0dafffd70af334c421
Agustin Gomez <gusmandoman@gmail.com>
2020-06-20 17:46:44 -0700

    rotating pickups

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public abstract class Pickup : MonoBehaviour
 {
+    public GameObject sprite;
+    public float angleSpeed;
     public abstract void action(GameObject Player);
-
+    private void Update() 
+    {
+        sprite.transform.RotateAround(sprite.transform.position,sprite.transform.up,angleSpeed);    
+    }
     private void OnCollisionEnter(Collision other) 
     {
         if(other.gameObject.tag == "Player")
