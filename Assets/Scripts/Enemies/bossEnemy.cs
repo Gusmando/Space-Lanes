c09a35d504f1272a8f10dd950966d815e6374adc
Agustin Gomez <gusmandoman@gmail.com>
2020-07-30 15:56:57 -0700

    Boss Animations Script

--- Assets/Scripts/Enemies/bossEnemy.cs
+++ Assets/Scripts/Enemies/bossEnemy.cs
@@ -8,19 +8,18 @@ public class bossEnemy : MonoBehaviour
     public GameObject rHand;
     public GameObject head;
     public Lane[] lanes;
-    
-    public bool leftShoot;
-    public bool headShoot;
-    public bool rightShoot;
     public bool fleeing;
     public bool attacking;
     public bool deciding;
-
     public int leftHandHp;
     public int headHp;
     public int rightHandHp;
     public int shootingMode;
-    public float decisionTime; 
+    public float decisionTime;
+
+    private Animator lHandAnim;
+    private Animator rHandAnim;
+    private Animator headAnim; 
 
     //Start is called before the first frame update
     void Start()
@@ -28,6 +27,9 @@ public class bossEnemy : MonoBehaviour
         head.transform.position = lanes[lanes.Length/2].position;
         lHand.transform.position = lanes[(lanes.Length/2) - 1].position;
         rHand.transform.position = lanes[(lanes.Length/2) + 1].position; 
+        lHandAnim = lHand.GetComponent<Animator>();
+        rHandAnim = rHand.GetComponent<Animator>();
+        headAnim = head.GetComponent<Animator>();
     }
 
     //Update is called once per frame
@@ -45,30 +47,49 @@ public class bossEnemy : MonoBehaviour
 
         if(deciding)
         {
-            shootingMode = Random.Range(1,3);
+            shootingMode = Random.Range(1,4);
+            switch (shootingMode) 
+            {
+                case 1:
+                    headAnim.SetInteger("animState",100);
+                    break;
+                case 2: 
+                    headAnim.SetInteger("animState",000);
+                    break;
+                case 3:
+                    headAnim.SetInteger("animState",001); 
+                    break;
+                default:
+                    break;
+            }
         }
         
-        switch (shootingMode) 
+        if(!deciding && attacking)
         {
-            case 1:
-                break;
-            case 2: 
-                break;
-            case 3: 
-                break;
-            case 4:
-                break;
-            case 5: 
-                break;
-            case 6: 
-                break;
-            case 7: 
-                break;
-            case 8: 
-                break;
-            default:
-                break;
+            switch (shootingMode) 
+            {
+                case 1:
+                    lHandAnim.SetBool("shooting",true);
+                    headAnim.SetInteger("animState",010);
+                    rHandAnim.SetBool("shooting",false);
+                    break;
+                case 2: 
+                    lHandAnim.SetBool("shooting",true);
+                    headAnim.SetInteger("animState",000);
+                    rHandAnim.SetBool("shooting",true);
+                    break;
+                case 3:
+                    lHandAnim.SetBool("shooting",false);
+                    headAnim.SetInteger("animState",010);
+                    rHandAnim.SetBool("shooting",true); 
+                    break;
+                default:
+                    break;
+            }
         }
+        
+
+
 
     }
 
